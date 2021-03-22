2d93d887c137b361e41c63d26eaa3f66899c780a
Timur Radko <timurradko@gmail.com>
2021-03-22 20:35:30 +0300

    refactoring isPositiveNumber in StringUtils class

--- lib/src/main/java/com/epam/esm/StringUtils.java
+++ lib/src/main/java/com/epam/esm/StringUtils.java
@@ -4,9 +4,11 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 import java.math.BigDecimal;
 
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (org.apache.commons.lang3.StringUtils.isNumeric(str)) {
+        if (isNumeric(str)) {
             BigDecimal number = NumberUtils.createBigDecimal(str);
             return number.compareTo(BigDecimal.ZERO) > 0;
         } else {
